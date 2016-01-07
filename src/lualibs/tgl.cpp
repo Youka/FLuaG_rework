2fc132e84d0937ac87efb7a4834812fa41015d13
Youka <spanknebel.borken@t-online.de>
2016-01-07 17:10:55 +0100

    began tgl implementation

--- src/lualibs/tgl.cpp
+++ src/lualibs/tgl.cpp
@@ -13,15 +13,92 @@ Permission is granted to anyone to use this software for any purpose, including 
 */
 
 #include "libs.h"
-#include <glfw3.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <mutex>
 
-int luaopen_tgl(lua_State* L){
-	// Initialize GLFW
-	if(!glfwInit())
-		return luaL_error(L, "Couldn't initialize GLFW!");
-	// Check GL for version 3.2+
+// Unique names for Lua metatables
+#define LUA_TGL_CONTEXT "tgl_context"
+#define LUA_TGL_SHADER "tgl_shader"
+#define LUA_TGL_PROGRAM "tgl_program"
+#define LUA_TGL_VAO "tgl_vao"
+#define LUA_TGL_TEXTURE "tgl_texture"
+#define LUA_TGL_FBO "tgl_fbo"
+
+// GL context management variables for thread-safety
+static std::mutex context_mutex;
+static unsigned context_count = 0;
+
+// Context metatable methods
+static int tgl_context_free(lua_State* L){
+	std::unique_lock<std::mutex> lock(context_mutex);
+	glfwDestroyWindow(*reinterpret_cast<GLFWwindow**>(luaL_checkudata(L, 1, LUA_TGL_CONTEXT)));
+	if(!--context_count)
+		glfwTerminate();
+	return 0;
+}
+
+static int tgl_context_activate(lua_State* L){
+	// Set current GL context to use
+	glfwMakeContextCurrent(*reinterpret_cast<GLFWwindow**>(luaL_checkudata(L, 1, LUA_TGL_CONTEXT)));
+	// Initialize GLEW
+	if(glewInit() != GLEW_OK)
+		return luaL_error(L, "Couldn't initialize GLEW!");
+	return 0;
+}
+
+// Shader metatable methods
+static int tgl_shader_create(lua_State* L){
+	const char* shader_type = luaL_checkstring(L, 1),
+		*shader_source = luaL_checkstring(L, 2);
+	GLuint shader = glCreateShader(GL_VERTEX_SHADER);
 
 	// TODO
 
-	return luaL_error(L, "TGL not implemented yet!");
+	*reinterpret_cast<GLuint*>(lua_newuserdata(L, sizeof(GLuint))) = shader;
+	if(luaL_newmetatable(L, LUA_TGL_SHADER)){
+
+		// TODO
+
+	}
+	lua_setmetatable(L, -2);
+	return 1;
+}
+
+int luaopen_tgl(lua_State* L){
+	// Thread-lock for safe GLFW usage
+	std::unique_lock<std::mutex> lock(context_mutex);
+	// Initialize GLFW with general properties
+	if(!context_count){
+		if(glfwInit() != GL_TRUE)
+			return luaL_error(L, "Couldn't initialize GLFW!");
+		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+		glfwWindowHint(GLFW_DECORATED, GL_FALSE);
+		glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	}
+	// Create GL context
+	GLFWwindow* window = glfwCreateWindow(1, 1, "tgl", nullptr, nullptr);
+	if(!window)
+		return luaL_error(L, "Couldn't create GL context!");
+	// Create userdata for GL context
+	GLFWwindow** udata = reinterpret_cast<GLFWwindow**>(lua_newuserdata(L, sizeof(GLFWwindow*)));
+	*udata = window;
+	// Fetch/create Lua tgl context metatable
+	if(luaL_newmetatable(L, LUA_TGL_CONTEXT)){
+		lua_pushcfunction(L, tgl_context_free); lua_setfield(L, -2, "__gc");
+		lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+		lua_pushcfunction(L, tgl_context_activate); lua_setfield(L, -2, "activate");
+		lua_pushcfunction(L, tgl_shader_create); lua_setfield(L, -2, "createshader");
+	}
+	// Bind metatable to userdata
+	lua_setmetatable(L, -2);
+	// Increment counter for finally created context
+	++context_count;
+	// Return the userdata to Lua
+	return 1;
 }
