b690a3553541a825b2c5dda39e44a46e054c77bf
Youka <spanknebel.borken@t-online.de>
2016-01-20 18:51:27 +0100

    implemented table tostring function

--- src/lualibs/tableex.cpp
+++ src/lualibs/tableex.cpp
@@ -14,6 +14,7 @@ Permission is granted to anyone to use this software for any purpose, including 
 
 #include "libs.h"
 #include <functional>
+#include <sstream>
 
 // Lua compatibility macros
 #if LUA_VERSION_NUM <= 501
@@ -42,7 +43,7 @@ static int table_copy(lua_State* L){
 			lua_pushvalue(L, -2);
 			lua_insert(L, -2);
 			// Set key+value to output table
-			if(lua_istable(L, -1) && deep)
+			if(deep && lua_istable(L, -1))
 				copy();
 			lua_rawset(L, -4);
 		}
@@ -58,12 +59,93 @@ static int table_copy(lua_State* L){
 }
 
 static int table_tostring(lua_State* L){
-	// Check main argument
+	// Check argument
 	luaL_checktype(L, 1, LUA_TTABLE);
-
-	// TODO
-
-	return 0;
+	// Remove unnecessary arguments
+	const int args_n = lua_gettop(L);
+	if(args_n > 1)
+		lua_pop(L, args_n - 1);
+	// Output buffer
+	std::ostringstream buf;
+	buf << std::boolalpha << std::showbase;
+	// Build table string in buffer
+	std::function<void(const unsigned)> build;
+	build = [&build,L,&buf](const unsigned indent){
+		// Indentation strings
+		std::string indent_str(indent, '\t'), indent_more_str(indent+1, '\t');
+		// Write table header
+		if(buf.tellp())
+			buf << '\n';
+		buf << indent_str << '{';
+		// Iterate through table elements
+		lua_pushnil(L);
+		while(lua_next(L, -2)){
+			// Write key
+			buf << '\n' << indent_more_str << '[';
+			switch(lua_type(L, -2)){
+				case LUA_TBOOLEAN:
+					buf << static_cast<bool>(lua_toboolean(L, -2));
+					break;
+				case LUA_TNUMBER:
+					buf << std::dec << lua_tonumber(L, -2);
+					break;
+                                case LUA_TSTRING:
+                                	buf << '"' << lua_tostring(L, -2) << '"';
+					break;
+				case LUA_TTABLE:
+				case LUA_TFUNCTION:
+				case LUA_TLIGHTUSERDATA:
+				case LUA_TUSERDATA:
+				case LUA_TTHREAD:
+					switch(lua_type(L, -2)){
+						case LUA_TTABLE: buf << "table:"; break;
+						case LUA_TFUNCTION: buf << "function:"; break;
+						case LUA_TLIGHTUSERDATA: buf << "lightuserdata:"; break;
+						case LUA_TUSERDATA: buf << "userdata:"; break;
+						case LUA_TTHREAD: buf << "thread:"; break;
+					}
+					buf << std::hex << lua_topointer(L, -2);
+					break;
+			}
+			buf << "] = ";
+			// Write value
+			switch(lua_type(L, -1)){
+				case LUA_TBOOLEAN:
+					buf << static_cast<bool>(lua_toboolean(L, -1));
+					break;
+				case LUA_TNUMBER:
+					buf << std::dec << lua_tonumber(L, -1);
+					break;
+                                case LUA_TSTRING:
+                                	buf << '"' << lua_tostring(L, -1) << '"';
+					break;
+				case LUA_TTABLE:
+					build(indent+1);
+					break;
+				case LUA_TFUNCTION:
+				case LUA_TLIGHTUSERDATA:
+				case LUA_TUSERDATA:
+				case LUA_TTHREAD:
+					switch(lua_type(L, -1)){
+						case LUA_TFUNCTION: buf << "function:"; break;
+						case LUA_TLIGHTUSERDATA: buf << "lightuserdata:"; break;
+						case LUA_TUSERDATA: buf << "userdata:"; break;
+						case LUA_TTHREAD: buf << "thread:"; break;
+					}
+					buf << std::hex << lua_topointer(L, -1);
+					break;
+			}
+			buf << ',';
+			// Remove value, keep key for next iteration pass
+			lua_pop(L, 1);
+		}
+		// Write table footer
+		buf << '\n' << indent_str << '}';
+	};
+	build(0);
+	// Return buffer string to Lua
+	lua_pushstring(L, buf.str().c_str());
+	return 1;
 }
 
 int luaopen_tableex(lua_State* L){
