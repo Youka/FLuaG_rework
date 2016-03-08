02b249b923ae7a6ba4d98dad94e40619c551da06
Youka <spanknebel.borken@t-online.de>
2016-03-08 01:43:25 +0100

    added Lua font listing on Linux

--- src/lualibs/font.cpp
+++ src/lualibs/font.cpp
@@ -13,10 +13,45 @@ Permission is granted to anyone to use this software for any purpose, including 
 */
 
 #include "libs.h"
+#ifndef _WIN32
+	#include <fontconfig/fontconfig.h>
+	#include <memory>
+#endif
 
-int luaopen_font(lua_State* L){
+static int font_list(lua_State* L){
+#ifdef _WIN32
 
 	// TODO
 
-	return luaL_error(L, "Font not implemented yet!");
+	return 0;
+#else
+	FcConfig* fc = FcInitLoadConfigAndFonts();
+	std::unique_ptr<FcPattern, void(*)(FcPattern*)> pattern(FcPatternCreate(), FcPatternDestroy);
+	std::unique_ptr<FcObjectSet, void(*)(FcObjectSet*)> objset(FcObjectSetBuild(FC_FAMILY, FC_STYLE, FC_FILE, FC_OUTLINE, nullptr), FcObjectSetDestroy);
+	std::unique_ptr<FcFontSet, void(*)(FcFontSet*)> fontlist(FcFontList(fc, pattern.get(), objset.get()), FcFontSetDestroy);
+	if(fontlist){
+		lua_createtable(L, fontlist->nfont, 0);
+		for(int i = 0; i < fontlist->nfont; ++i){
+			lua_createtable(L, 0, 4);
+			const FcPattern* font = fontlist->fonts[i];
+			FcChar8* sresult; FcBool bresult;
+			FcPatternGetString(font, FC_FAMILY, 0, &sresult); lua_pushstring(L, reinterpret_cast<char*>(sresult)); lua_setfield(L, -2, "family");
+			FcPatternGetString(font, FC_STYLE, 0, &sresult); lua_pushstring(L, reinterpret_cast<char*>(sresult)); lua_setfield(L, -2, "style");
+			FcPatternGetString(font, FC_FILE, 0, &sresult); lua_pushstring(L, reinterpret_cast<char*>(sresult)); lua_setfield(L, -2, "file");
+			FcPatternGetBool(font, FC_OUTLINE, 0, &bresult); lua_pushboolean(L, bresult); lua_setfield(L, -2, "outline");
+			lua_rawseti(L, -2, 1+i);
+		}
+		return 1;
+	}else
+		return 0;
+#endif
+}
+
+int luaopen_font(lua_State* L){
+	static const luaL_Reg l[] = {
+		{"list", font_list},
+		{NULL, NULL}
+	};
+	luaL_newlib(L, l);
+	return 1;
 }
