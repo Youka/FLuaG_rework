1ff9606bc9ecdf58235f4a2359358c5fa29bca8d
Youka <spanknebel.borken@t-online.de>
2016-03-08 09:00:54 +0100

    fixed test to old C standard

--- tests/hello.c
+++ tests/hello.c
@@ -1,22 +1,25 @@
-// DLL functions
+/* DLL functions */
 #include "dl.h"
 
-// Program entry
+/* Program entry */
 int main(const int argc, const char** argv){
-	// Check for input
-	if(argc != 2)
-		return 1;
-	// Open plugin handle
-	DLL_HANDLE plugin = DLL_OPEN(argv[1]);
-	if(!plugin)
-		return 2;
-	// Get plugin functions
+	/* Declarations */
+	DLL_HANDLE plugin;
 	typedef void* fluag_h;
 	fluag_h(*fluag_create)(void);
 	int(*fluag_load_script)(fluag_h, const char*, char*);
 	void(*fluag_set_userdata)(fluag_h, const char*);
 	void(*fluag_destroy)(fluag_h);
 	const char*(*fluag_get_version)(void);
+	fluag_h f;
+	/* Check for input */
+	if(argc != 2)
+		return 1;
+	/* Open plugin handle */
+	plugin = DLL_OPEN(argv[1]);
+	if(!plugin)
+		return 2;
+	/* Get plugin functions */
 #ifdef __GNUC__
 	#pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
 #endif
@@ -30,24 +33,24 @@ int main(const int argc, const char** argv){
 		DLL_CLOSE(plugin);
 		return 3;
 	}
-	// Create FLuaG instance
-	fluag_h f = fluag_create();
+	/* Create FLuaG instance */
+	f = fluag_create();
 	if(!f){
 		DLL_CLOSE(plugin);
 		return 4;
 	}
-	// Set userdata to instance
+	/* Set userdata to instance */
 	fluag_set_userdata(f, fluag_get_version());
-	// Load script to instance
+	/* Load script to instance */
 	if(!fluag_load_script(f, "print(string.format('Hello from FLuaG v%s!', ...))", 0)){
 		fluag_destroy(f);
 		DLL_CLOSE(plugin);
 		return 5;
 	}
-	// Destroy instance
+	/* Destroy instance */
 	fluag_destroy(f);
-	// Close plugin handle
+	/* Close plugin handle */
 	DLL_CLOSE(plugin);
-	// Successful program end
+	/* Successful program end */
 	return 0;
 }
