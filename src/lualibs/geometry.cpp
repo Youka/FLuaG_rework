32547a166debd85f69db78d65ca67786e0d875f2
Youka <spanknebel.borken@t-online.de>
2016-03-10 21:39:29 +0100

    added GLU tesselation to Lua geometry

--- src/lualibs/geometry.cpp
+++ src/lualibs/geometry.cpp
@@ -15,6 +15,8 @@ Permission is granted to anyone to use this software for any purpose, including 
 #include "libs.h"
 #include "../utils/lua.h"
 #include "../utils/math.hpp"
+#include <GL/glu.h>
+#include <memory>
 
 static int geometry_stretch(lua_State* L){
 	const Geometry::Point2d v = Geometry::stretch({luaL_checknumber(L, 1), luaL_checknumber(L,2)}, luaL_checknumber(L, 3));
@@ -90,31 +92,6 @@ static int geometry_arc_curve(lua_State* L){
 	return 1;
 }
 
-static int geometry_ear_clipping(lua_State* L){
-	// Check argument
-	luaL_checktype(L, 1, LUA_TTABLE);
-	// Get argument (table) as 2d points
-	std::vector<Geometry::Point2d> points(lua_rawlen(L, 1) >> 1);
-	int i = 0;
-	for(auto& point : points){
-		lua_rawgeti(L, 1, ++i); lua_rawgeti(L, 1, ++i);
-                point = {luaL_checknumber(L, -2), luaL_checknumber(L, -1)};
-		lua_pop(L, 2);
-	}
-	// Convert polygon/points to triangles
-	const auto triangles = Geometry::ear_clipping(points);
-	// Send triangles to Lua
-	lua_createtable(L, triangles.size(), 0);
-	i = 0;
-	for(const auto& triangle : triangles){
-		lua_createtable(L, 6, 0);
-		lua_pushnumber(L, triangle[0].x); lua_pushnumber(L, triangle[0].y); lua_pushnumber(L, triangle[1].x); lua_pushnumber(L, triangle[1].y); lua_pushnumber(L, triangle[2].x); lua_pushnumber(L, triangle[2].y);
-		lua_rawseti(L, -7, 6); lua_rawseti(L, -6, 5); lua_rawseti(L, -5, 4); lua_rawseti(L, -4, 3); lua_rawseti(L, -3, 2); lua_rawseti(L, -2, 1);
-		lua_rawseti(L, -2, ++i);
-	}
-	return 1;
-}
-
 static int geometry_curve_flatten(lua_State* L){
 	try{
 		const auto lines = Geometry::curve_flatten({{
@@ -134,6 +111,110 @@ static int geometry_curve_flatten(lua_State* L){
 		return luaL_error(L, e.what());
 	}
 	return 0;
+}
+
+static int geometry_ear_clipping(lua_State* L){
+	// Check argument
+	luaL_checktype(L, 1, LUA_TTABLE);
+	// Get argument (table) as 2d points
+	std::vector<Geometry::Point2d> points(lua_rawlen(L, 1) >> 1);
+	int i = 0;
+	for(auto& point : points){
+		lua_rawgeti(L, 1, ++i); lua_rawgeti(L, 1, ++i);
+		point = {luaL_checknumber(L, -2), luaL_checknumber(L, -1)};
+		lua_pop(L, 2);
+	}
+	// Convert polygon/points to triangles
+	const auto triangles = Geometry::ear_clipping(points);
+	// Send triangles to Lua
+	lua_createtable(L, triangles.size(), 0);
+	i = 0;
+	for(const auto& triangle : triangles){
+		lua_createtable(L, 6, 0);
+		lua_pushnumber(L, triangle[0].x); lua_pushnumber(L, triangle[0].y); lua_pushnumber(L, triangle[1].x); lua_pushnumber(L, triangle[1].y); lua_pushnumber(L, triangle[2].x); lua_pushnumber(L, triangle[2].y);
+		lua_rawseti(L, -7, 6); lua_rawseti(L, -6, 5); lua_rawseti(L, -5, 4); lua_rawseti(L, -4, 3); lua_rawseti(L, -3, 2); lua_rawseti(L, -2, 1);
+		lua_rawseti(L, -2, ++i);
+	}
+	return 1;
+}
+
+static int geometry_tesselate(lua_State* L){
+	// Check argument
+	luaL_checktype(L, 1, LUA_TTABLE);
+	// Get argument (table) as contours of 2d/fake-3d points
+	std::vector<std::vector<std::array<double,3>>> contours(lua_rawlen(L, 1));
+	for(size_t contour_i = 0; contour_i < contours.size(); ++contour_i){
+		std::vector<std::array<double,3>>& points = contours[contour_i];
+		lua_rawgeti(L, 1, 1+contour_i);
+		luaL_checktype(L, -1, LUA_TTABLE);
+		points.resize(lua_rawlen(L, -1) >> 1);
+		int i = 0;
+		for(auto& point : points){
+			lua_rawgeti(L, -1, ++i); lua_rawgeti(L, -2, ++i);
+			point = {luaL_checknumber(L, -2), luaL_checknumber(L, -1), 0};
+			lua_pop(L, 2);
+		}
+		lua_pop(L, 1);
+	}
+	// Tesselate with GLU
+	struct TessState{
+		GLenum cur_type;
+		std::vector<Geometry::Point2d> buffer;
+		std::vector<std::array<Geometry::Point2d,3>> triangles;
+	}state;
+	const std::unique_ptr<GLUtesselator, void(APIENTRY *)(GLUtesselator*)> tess(gluNewTess(), gluDeleteTess);
+	gluTessProperty(tess.get(), GLU_TESS_WINDING_RULE, GLU_TESS_WINDING_ODD);
+	gluTessProperty(tess.get(), GLU_TESS_BOUNDARY_ONLY, GL_FALSE);
+	void (*begin_callback)(const GLenum, void*) = [](const GLenum type, void* userdata){
+		static_cast<TessState*>(userdata)->cur_type = type;
+	};
+	gluTessCallback(tess.get(), GLU_TESS_BEGIN_DATA, reinterpret_cast<void(APIENTRY *)()>(begin_callback));
+	void (*vertex_callback)(const void*, void*) = [](const void* vertex, void* userdata){
+		auto& pvertex = *static_cast<const std::array<double,3>*>(vertex);
+		static_cast<TessState*>(userdata)->buffer.push_back({pvertex[0], pvertex[1]});
+	};
+	gluTessCallback(tess.get(), GLU_TESS_VERTEX_DATA, reinterpret_cast<void(APIENTRY *)()>(vertex_callback));
+	void (*end_callback)(void*) = [](void* userdata){
+		TessState* state = static_cast<TessState*>(userdata);
+		switch(state->cur_type){
+			case GL_TRIANGLES:
+				for(size_t i = 0; i+2 < state->buffer.size(); i+=3)
+					state->triangles.push_back({state->buffer[i], state->buffer[i+1], state->buffer[i+2]});
+				break;
+			case GL_TRIANGLE_STRIP:
+				for(size_t i = 2; i < state->buffer.size(); ++i)
+					state->triangles.push_back({state->buffer[i-2], state->buffer[i-1], state->buffer[i]});
+				break;
+			case GL_TRIANGLE_FAN:
+				for(size_t i = 2; i < state->buffer.size(); ++i)
+					state->triangles.push_back({state->buffer.front(), state->buffer[i-1], state->buffer[i]});
+				break;
+		}
+		state->buffer.clear();
+	};
+	gluTessCallback(tess.get(), GLU_TESS_END_DATA, reinterpret_cast<void(APIENTRY *)()>(end_callback));
+	void (*combine_callback)(const GLdouble[3], const void*[4], const GLfloat[4], void**) = [](const GLdouble coords[3], const void*[4], const GLfloat[4], void** out){
+		*out = new std::array<double,3>{coords[0], coords[1], coords[2]};	// Safe because std::array is just a wrapper around C array
+	};
+	gluTessCallback(tess.get(), GLU_TESS_COMBINE, reinterpret_cast<void(APIENTRY *)()>(combine_callback));
+	gluTessBeginPolygon(tess.get(), &state);
+	for(auto& contour : contours){
+		gluTessBeginContour(tess.get());
+		for(auto& points : contour)
+			gluTessVertex(tess.get(), points.data(), points.data());
+		gluTessEndContour(tess.get());
+	}
+	gluTessEndPolygon(tess.get());
+	// Send triangles to Lua
+	lua_createtable(L, state.triangles.size(), 0);
+	int i = 0;
+	for(const auto& triangle : state.triangles){
+		lua_createtable(L, 6, 0);
+		lua_pushnumber(L, triangle[0].x); lua_pushnumber(L, triangle[0].y); lua_pushnumber(L, triangle[1].x); lua_pushnumber(L, triangle[1].y); lua_pushnumber(L, triangle[2].x); lua_pushnumber(L, triangle[2].y);
+		lua_rawseti(L, -7, 6); lua_rawseti(L, -6, 5); lua_rawseti(L, -5, 4); lua_rawseti(L, -4, 3); lua_rawseti(L, -3, 2); lua_rawseti(L, -2, 1);
+		lua_rawseti(L, -2, ++i);
+	}
+	return 1;
 }
 
 #define LUA_MATRIX "matrix"
@@ -265,8 +346,9 @@ int luaopen_geometry(lua_State* L){
 		{"direction", geometry_direction},
 		{"bound", geometry_bound},
 		{"arccurve", geometry_arc_curve},
-		{"earclipping", geometry_ear_clipping},
 		{"curveflatten", geometry_curve_flatten},
+		{"earclipping", geometry_ear_clipping},
+		{"tesselate", geometry_tesselate},
 		{"matrix", geometry_matrix_create},
 		{NULL, NULL}
 	};
